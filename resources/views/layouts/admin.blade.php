6b198558446afed90bcd0c93fd6ae6b80e1f5fcc
Efril Mulanda <efril.mulanda@gmail.com>
2022-08-25 00:33:06 +0700

    Dashboard and Category Model

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -12,6 +12,7 @@
     @stack('prepend-style')
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
     <link href="{{ url('style/main.css') }}" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/dt-1.12.1/datatables.min.css" />
     @stack('addon-style')
 </head>
 
@@ -24,7 +25,7 @@
                     <img src="/images/admin.png" alt="" class="my-4" />
                 </div>
                 <div class="list-group list-group-flush">
-                    <a href="/dashboard.html" class="list-group-item list-group-item-action active">
+                    <a href="{{ route('admin-dashboard') }}" class="list-group-item list-group-item-action active">
                         Dashboard
                     </a>
                     <a href="/dashboard-products.html" class="list-group-item list-group-item-action">
@@ -101,8 +102,9 @@
 
     <!-- Bootstrap core JavaScript -->
     @stack('prepend-script')
-    <script src="/vendor/jquery/jquery.slim.min.js"></script>
+    <script src="/vendor/jquery/jquery.min.js"></script>
     <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.12.1/datatables.min.js"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script>
         AOS.init();
